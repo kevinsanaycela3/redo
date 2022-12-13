45ca6c7f4f1ea55b8a8bea8f56fd5ab9455e83dc
Kevin Sanaycela <kevinsanaycela@Kevins-MacBook-Air.local>
2022-12-13 14:39:34 -0500

    local

--- terraform/install_docker.sh
+++ terraform/install_docker.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
 cd /home/ubuntu
-
 git clone https://github.com/kevinsanaycela3/redo.git
+cd redo/ifme/
+
+#INSTALLING POSTGRESQL
+sudo apt install wget ca-certificates
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+sudo apt update
+sudo apt install postgresql postgresql-contrib -y
+
+#INSTALLING RUBY ON RAILS
+
+#Install yarn 
+sudo apt-get install curl -y
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt update -y && sudo apt install yarn -y 
+bash < <(curl -sL https://raw.github.com/railsgirls/installation-scripts/master/rails-install-ubuntu.sh)
+
+#Gems
+
+
+
+
+
 
 
 
