4a965c07cb075ee9c07f06fbbed635c916dfc65e
Kevin Sanaycela <kevinsanaycela@Kevins-MacBook-Air.local>
2022-12-14 10:27:43 -0500

    revert install_docker.sh

--- terraform/install_docker.sh
+++ terraform/install_docker.sh
@@ -2,3 +2,16 @@
 
 cd /home/ubuntu
 git clone https://github.com/kevinsanaycela3/redo.git
+
+cd redo/ifme/
+
+sudo apt-get update 
+sudo snap install docker 
+sudo snap start docker 
+
+
+sudo docker-compose build
+sudo docker-compose run app rake db:create db:migrate 
+sleep 3
+sudo docker-compose run app rake db:seed
+sudo docker-compose up
