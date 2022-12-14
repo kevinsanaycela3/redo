49a3d85605efc8c3aeaec59004fc154d8d7d749c
Kevin Sanaycela <kevinsanaycela@Kevins-MacBook-Air.local>
2022-12-14 13:43:33 -0500

    fina;

--- terraform/install_docker.sh
+++ terraform/install_docker.sh
@@ -28,8 +28,4 @@ sudo docker-compose run app rake db:create db:migrate
 sleep 3
 sudo docker-compose run app rake db:seed
 sudo docker-compose up 
-
-sleep 120
-
-#Remove containers
-sudo docker-compose down 
+ 
