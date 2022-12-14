df0a2f031c4ac1e79600b256d2b7e6c7931e08c3
Kevin Sanaycela <kevinsanaycela@Kevins-MacBook-Air.local>
2022-12-14 13:18:21 -0500

    public ip

--- ifme/config/environments/development.rb
+++ ifme/config/environments/development.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-primary_domain = '3.88.103.229:3000'
+primary_domain = '54.242.47.187:3000'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
