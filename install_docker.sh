6968bcfc99f838685caf443f598492de6b390310
kevinsanaycela3 <62778529+kevinsanaycela3@users.noreply.github.com>
2022-12-15 16:54:39 -0500

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -16,7 +16,7 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 sudo apt install docker-compose -y 
 
-#Build Images for Application and Database
+#Build Images for Application and Database YO
 cd
 cd staging_environment/
 ./install_app.sh
